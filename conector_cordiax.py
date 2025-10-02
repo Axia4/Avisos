dc8115d75fb1ba1caacff0cbc440240bb88e2b94
Naiel <109038805+naielv@users.noreply.github.com>
2025-10-02 15:44:38 +0200

    disable offset

--- conector_cordiax.py
+++ conector_cordiax.py
@@ -106,7 +106,7 @@ def show_notification(msg, click_url=None, custom_title=None, priority=3):
     with popup_lock:
         y_offset = 50 + popup_offset
         popup_offset += 40
-    popup.geometry(f"+100+{y_offset}")
+    # popup.geometry(f"+100+{y_offset}")
 
     # High-priority flashing banner
     if priority > 3:
